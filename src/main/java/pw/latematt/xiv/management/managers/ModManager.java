8ed26b071e382108b878afdec00ca949c23dcc82
TehNeon <tehneon1212@gmail.com>
2015-09-24 17:46:08 -0400

    Added a special people command

--- src/main/java/pw/latematt/xiv/management/managers/ModManager.java
+++ src/main/java/pw/latematt/xiv/management/managers/ModManager.java
@@ -1,11 +1,14 @@
 package pw.latematt.xiv.management.managers;
 
 import pw.latematt.xiv.XIV;
+import pw.latematt.xiv.command.Command;
 import pw.latematt.xiv.management.ListManager;
 import pw.latematt.xiv.mod.Mod;
 import pw.latematt.xiv.mod.mods.HUD;
+import pw.latematt.xiv.utils.ChatLogger;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Matthew
@@ -20,5 +23,19 @@ public class ModManager extends ListManager<Mod> {
         XIV.getInstance().getLogger().info("Starting to setup " + getClass().getSimpleName() + "...");
         contents.add(new HUD());
         XIV.getInstance().getLogger().info("Successfully setup " + getClass().getSimpleName() + ".");
+
+
+        Command.newCommand()
+                .cmd("mods")
+                .description("Provides help with commands.")
+                .aliases("modules", "hacks", "cheats")
+                .handler(message -> {
+                    List<Mod> moduleList = XIV.getInstance().getModManager().getContents();
+                    StringBuilder mods = new StringBuilder("Modules (" + moduleList.size() + "): ");
+                    for (Mod mod : moduleList) {
+                        mods.append(mod.isEnabled() ? "\247a" : "\247c").append(mod.getName()).append("\247r, ");
+                    }
+                    ChatLogger.print(mods.toString().substring(0, mods.length() - 2));
+                }).build();
     }
 }
