66f09f15d56fefe19a1d89567a0d4738c00a80b4
TehNeon <tehneon1212@gmail.com>
2015-09-27 18:28:58 -0400

    Stick a dick in your ass Martin

---
+++ src/main/java/pw/latematt/xiv/mod/mods/Fly.java
@@ -0,0 +1,56 @@
+package pw.latematt.xiv.mod.mods;
+
+import org.lwjgl.input.Keyboard;
+import pw.latematt.xiv.XIV;
+import pw.latematt.xiv.command.Command;
+import pw.latematt.xiv.command.CommandHandler;
+import pw.latematt.xiv.event.Listener;
+import pw.latematt.xiv.event.events.MotionUpdateEvent;
+import pw.latematt.xiv.event.events.SendPacketEvent;
+import pw.latematt.xiv.mod.Mod;
+import pw.latematt.xiv.mod.ModType;
+
+/**
+ * @author TehNeon
+ */
+public class Fly extends Mod implements Listener<MotionUpdateEvent>, CommandHandler {
+    public Fly() {
+        super("Fly", ModType.PLAYER, Keyboard.KEY_NONE, 0xFF4b97f6);
+
+        Command.newCommand()
+                .cmd("fly")
+                .aliases("flight")
+                .description("FLIGHT FLY FLIGHT")
+                .arguments("<action>")
+                .handler(this).build();
+    }
+
+    public void onEventCalled(MotionUpdateEvent event) {
+        double motionX = 0, motionY = 0, motionZ = 0;
+
+        if(mc.gameSettings.keyBindJump.getIsKeyPressed()) {
+            motionY = 0.5;
+        }
+
+        if(mc.gameSettings.keyBindSneak.getIsKeyPressed()) {
+            motionY = -0.5;
+        }
+
+        mc.thePlayer.motionY = motionY;
+    }
+
+    @Override
+    public void onCommandRan(String message) {
+
+    }
+
+    @Override
+    public void onEnabled() {
+        XIV.getInstance().getListenerManager().add(this);
+    }
+
+    @Override
+    public void onDisabled() {
+        XIV.getInstance().getListenerManager().remove(this);
+    }
+}
