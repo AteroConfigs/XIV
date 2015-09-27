ab9c3934b4cf67021c62983046c308b04f6ee9f6
TehNeon <tehneon1212@gmail.com>
2015-09-27 15:24:13 -0400

    Added Inv Walk

---
+++ src/main/java/pw/latematt/xiv/mod/mods/InventoryWalk.java
@@ -0,0 +1,63 @@
+package pw.latematt.xiv.mod.mods;
+
+import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.GuiNewChat;
+import net.minecraft.client.settings.KeyBinding;
+import org.lwjgl.input.Keyboard;
+import pw.latematt.xiv.XIV;
+import pw.latematt.xiv.command.Command;
+import pw.latematt.xiv.command.CommandHandler;
+import pw.latematt.xiv.event.Listener;
+import pw.latematt.xiv.event.events.MotionUpdateEvent;
+import pw.latematt.xiv.event.events.SendPacketEvent;
+import pw.latematt.xiv.mod.Mod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TehNeon
+ */
+public class InventoryWalk extends Mod implements Listener<MotionUpdateEvent> {
+
+    public InventoryWalk() {
+        super("Inventory Walk");
+    }
+
+    public void onEventCalled(MotionUpdateEvent event) {
+        if (mc.currentScreen == null) return;
+        if (mc.currentScreen instanceof GuiChat) return;
+
+        KeyBinding[] keyBindings = {mc.gameSettings.keyBindForward, mc.gameSettings.keyBindBack, mc.gameSettings.keyBindLeft, mc.gameSettings.keyBindRight, mc.gameSettings.keyBindJump};
+
+        for (KeyBinding keyBinding : keyBindings) {
+            keyBinding.setKeyBindState(keyBinding.getKeyCode(), Keyboard.isKeyDown(keyBinding.getKeyCode()));
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+            mc.thePlayer.rotationPitch--;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+            mc.thePlayer.rotationPitch++;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+            mc.thePlayer.rotationYaw--;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+            mc.thePlayer.rotationYaw++;
+        }
+    }
+
+    @Override
+    public void onEnabled() {
+        XIV.getInstance().getListenerManager().add(this);
+    }
+
+    @Override
+    public void onDisabled() {
+        XIV.getInstance().getListenerManager().remove(this);
+    }
+}
