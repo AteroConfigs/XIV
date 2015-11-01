6af8d11ab8d96aefc844683106ae69e1cbc1c154
TehNeon <tehneon1212@gmail.com>
2015-11-01 18:23:47 -0500

    CHANGED INVWALK

--- src/main/java/pw/latematt/xiv/mod/mods/movement/InventoryWalk.java
+++ src/main/java/pw/latematt/xiv/mod/mods/movement/InventoryWalk.java
@@ -38,22 +38,10 @@ public class InventoryWalk extends Mod implements Listener<MotionUpdateEvent> {
 
         if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
             mc.thePlayer.rotationYaw -= 4;
-
-            if (mc.thePlayer.rotationYaw < 180.0F) {
-                mc.thePlayer.rotationYaw += 360.0F;
-            } else if (mc.thePlayer.rotationYaw > 180.0F) {
-                mc.thePlayer.rotationYaw -= 360.0F;
-            }
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
             mc.thePlayer.rotationYaw += 4;
-
-            if (mc.thePlayer.rotationYaw < 180.0F) {
-                mc.thePlayer.rotationYaw += 360.0F;
-            } else if (mc.thePlayer.rotationYaw > 180.0F) {
-                mc.thePlayer.rotationYaw -= 360.0F;
-            }
         }
 
         if (mc.currentScreen instanceof GuiEditSign || mc.currentScreen instanceof GuiScreenBook || mc.currentScreen instanceof GuiRepair)
