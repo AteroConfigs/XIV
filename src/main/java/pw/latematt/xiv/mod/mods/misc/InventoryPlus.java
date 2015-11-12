512dc3342dbbbfb488867d942b6c38dbabc8bf42
Matthew <latematt420@outlook.com>
2015-11-11 19:05:21 -0500

    added inventoryplus

---
+++ src/main/java/pw/latematt/xiv/mod/mods/misc/InventoryPlus.java
@@ -0,0 +1,77 @@
+package pw.latematt.xiv.mod.mods.misc;
+
+import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.GuiRepair;
+import net.minecraft.client.gui.GuiScreenBook;
+import net.minecraft.client.gui.inventory.GuiEditSign;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.network.play.client.C0DPacketCloseWindow;
+import net.minecraft.util.MathHelper;
+import org.lwjgl.input.Keyboard;
+import pw.latematt.xiv.XIV;
+import pw.latematt.xiv.event.Listener;
+import pw.latematt.xiv.event.events.MotionUpdateEvent;
+import pw.latematt.xiv.event.events.SendPacketEvent;
+import pw.latematt.xiv.mod.Mod;
+import pw.latematt.xiv.mod.ModType;
+
+/**
+ * @author Matthew
+ * @author TehNeon
+ */
+public class InventoryPlus extends Mod {
+    private final Listener sendPacketListener, motionUpdateListener;
+    public InventoryPlus() {
+        super("InventoryPlus", ModType.MISCELLANEOUS);
+        setDisplayName("Inventory+");
+
+        sendPacketListener = new Listener<SendPacketEvent>() {
+            @Override
+            public void onEventCalled(SendPacketEvent event) {
+                event.setCancelled(event.getPacket() instanceof C0DPacketCloseWindow);
+            }
+        };
+
+        motionUpdateListener = new Listener<MotionUpdateEvent>() {
+            @Override
+            public void onEventCalled(MotionUpdateEvent event) {
+                if (event.getCurrentState() == MotionUpdateEvent.State.PRE) {
+                    if (mc.currentScreen == null)
+                        return;
+                    if (mc.currentScreen instanceof GuiChat)
+                        return;
+
+                    if (Keyboard.isKeyDown(Keyboard.KEY_UP))
+                        mc.thePlayer.rotationPitch = MathHelper.clamp_float(mc.thePlayer.rotationPitch - 4, -90.0F, 90.0F);
+                    if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+                        mc.thePlayer.rotationPitch = MathHelper.clamp_float(mc.thePlayer.rotationPitch + 4, -90.0F, 90.0F);
+                    if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
+                        mc.thePlayer.rotationYaw -= 4;
+                    if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
+                        mc.thePlayer.rotationYaw += 4;
+
+                    if (mc.currentScreen instanceof GuiEditSign || mc.currentScreen instanceof GuiScreenBook || mc.currentScreen instanceof GuiRepair)
+                        return;
+
+                    KeyBinding[] keyBindings = {
+                            mc.gameSettings.keyBindForward, mc.gameSettings.keyBindBack, mc.gameSettings.keyBindLeft, mc.gameSettings.keyBindRight,
+                            mc.gameSettings.keyBindJump, mc.gameSettings.keyBindSneak};
+
+                    for (KeyBinding keyBinding : keyBindings) {
+                        KeyBinding.setKeyBindState(keyBinding.getKeyCode(), Keyboard.isKeyDown(keyBinding.getKeyCode()));
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onEnabled() {
+        XIV.getInstance().getListenerManager().add(sendPacketListener, motionUpdateListener);
+    }
+
+    @Override
+    public void onDisabled() {
+        XIV.getInstance().getListenerManager().remove(sendPacketListener, motionUpdateListener);
+    }
+}
