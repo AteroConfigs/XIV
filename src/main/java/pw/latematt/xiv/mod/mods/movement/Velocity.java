d2e609df6f09d239305cd88951d49cb53cbd45b6
TehNeon <tehneon1212@gmail.com>
2015-11-13 13:20:32 -0500

    Changed velocity

--- src/main/java/pw/latematt/xiv/mod/mods/movement/Velocity.java
+++ src/main/java/pw/latematt/xiv/mod/mods/movement/Velocity.java
@@ -31,6 +31,8 @@ public class Velocity extends Mod implements CommandHandler {
         readPacketListener = new Listener<ReadPacketEvent>() {
             @Override
             public void onEventCalled(ReadPacketEvent event) {
+                updateTag();
+
                 if (event.getPacket() instanceof S12PacketEntityVelocity) {
                     S12PacketEntityVelocity packet = (S12PacketEntityVelocity) event.getPacket();
 
@@ -90,7 +92,7 @@ public class Velocity extends Mod implements CommandHandler {
                         String newVelocityString = arguments[2];
                         try {
                             float newPercent = arguments[2].equalsIgnoreCase("-d") ? reduction.getDefault() : Float.parseFloat(newVelocityString);
-                            reduction.setValue((newPercent / 100));
+                            reduction.setValue((newPercent));
                             if (reduction.getValue() > reduction.getMax())
                                 reduction.setValue(reduction.getMax());
                             else if (reduction.getValue() < reduction.getMin())
@@ -120,7 +122,7 @@ public class Velocity extends Mod implements CommandHandler {
             setTag("");
         } else {
             setDisplayName(getName());
-            setTag((reduction.getValue() * 100) + "%");
+            setTag((reduction.getValue()) + "%");
         }
     }
 
